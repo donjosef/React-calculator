0dfa57797e3b0aa946a90427222801d3621297aa
donjoe <giumontanaro@gmail.com>
2018-09-12 12:52:12 +0200

    create display and commands to use inside calculator

---
+++ src/components/Commands/Commands.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const commands = props => {
+    return (
+        <div>
+            mycommands
+        </div>
+    );
+}
+
+export default commands;
---
+++ src/components/Display/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const display = props => {
+    return (
+        <div>
+            {props.input}
+            8
+        </div>
+    );
+}
+
+export default display;
